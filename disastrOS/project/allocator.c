16d2df83df4cf22f24cae2353b7fee5754449b1b
Gianluca Scanu <scanu.1884094@studenti.uniroma1.it>
2023-02-06 23:54:17 +0100

    restyling completo

--- disastrOS/project/allocator.c
+++ disastrOS/project/allocator.c
@@ -31,7 +31,9 @@ int required_level(buddy_allocator* alloc, int size){
     for (int i = alloc->levels; i >= 1 ; i--){
 
         if (size <= threshold){
+
             return i-1;
+
         } 
 
         threshold = threshold << 1;
@@ -47,8 +49,11 @@ int required_level(buddy_allocator* alloc, int size){
 int set_ancestors(buddy_allocator* alloc, int bit){
 
     while (bit > 0){
+
         bit = parent(bit);
+
         bitmap_set_bit(alloc->bit_map, bit, 1);
+
     }
 
     return 0;
@@ -97,38 +102,44 @@ void * buddy_malloc(buddy_allocator* alloc, int size){
 
     if (size > alloc->memory_size) return block;
 
-    //we want to know which level we want to ask a buddy (block)
+    //we want to know to which level we want to ask a buddy (block)
     level = required_level(alloc,size);
 
     //gets the index of the first node of level
     first_node = first_of_level(level);
 
-    //finds and stores in free_node the first free buddy (block) able to contain size
+    //the loop finds and stores in free_node the first free buddy (block) able to contain size
     free_node = -1;
+
     for (int i = 0; i < nodes_in_level(level); i++){
+
         if (!bitmap_bit(alloc->bit_map, i + first_node)){
-            //il bit considerato è 0     
+    
             if(!search_ancestors(alloc, i + first_node, &parent_node)){
                 free_node = i + first_node;
                 break;
             }
-            //if the loop does not break it is not free, so i jumps to the next possible free block 
+
+            //if the loop does not break it is not free, so "i" jumps to the next possible free block 
             i = i  + (1 << (get_level(parent_node) - level));
         }
     }
+
+    //returns NULL since there is NOT ENOUGH FREE MEMORY
     if (free_node == -1){
-        //Returns NULL since there is NOT ENOUGH FREE MEMORY
+        
     }else{
-        //Gets the chunk size of the required level
+
+        //gets the chunk size of the required level
         int level_chunk_size = alloc->chunk_size << (alloc->levels - level - 1);
 
-        //Sets as taken the buddy requested and  his ancestors 
+        //sets as taken the buddy requested and  his ancestors 
         bitmap_set_bit(alloc->bit_map, free_node, 1);
         set_ancestors(alloc, free_node);
         
-
-        //Gets the address in memory corresponding to the free block
+        //gets the address in memory corresponding to the free block
         block = (void*)alloc->actual_memory + ((free_node - first_node)*level_chunk_size);
+
     }
     
     return block;
@@ -136,7 +147,20 @@ void * buddy_malloc(buddy_allocator* alloc, int size){
 
 
 
-//initialize a buddy allocator using "levels" levels for buddies
+/*
+     initializes a buddy allocator using "levels" levels for buddies and "min_chunk_size"
+        for the size of the lowest level's buddies. 
+    -returns 1 if : the buddy allocator's initialization has NOT been completed.
+    -returns 0 if : the buddy allocator's initialization has been completed without problems.
+    -returns -1 if : the buddy allocator's initialization has been completed but the number
+                     of levels was decreased to the maximum value of 16.
+    -returns -2 if : the buddy allocator's initialization has not been completed
+                     regularly since memory and block values were inconsistent.
+    -returns -3 if : the buddy's allocator's initialization has not been completed regularly
+                     since memory and block values were inconsistent and
+                     the number of levels was decreased to the maximum value of 16.
+
+*/
 int buddy_init(char* buffer, int buffer_size, int levels, buddy_allocator* alloc, int min_chunk_size,
                 char* memory, int memory_size){
 
@@ -149,47 +173,66 @@ int buddy_init(char* buffer, int buffer_size, int levels, buddy_allocator* alloc
 
     //checks if given levels number in input is over the limit
     if (levels > MAX_LEVELS){
+
         levels = MAX_LEVELS;
         err = -1;
+
     } 
 
-    //checks if the memory size is enough for the given minimum chunk's size given
-    //(1 << (levels - 1)) is the number of leaves of size min_chunk_size
+    /*checks if the memory size is enough for the given minimum chunk's size given
+        (1 << (levels - 1)) is the number of leaves of size min_chunk_size */
     if (memory_size < min_chunk_size * (1 << (levels - 1))) return 1;
 
-    if (memory_size != min_chunk_size * (1 << (levels - 1))){
+    if (memory_size != min_chunk_size * (1 << (levels - 1))){ 
+
         if (err == -1){
+
             err = -3;
+
         }else{
-            //Memory input not correct
+
             err = -2;
+
         }
     }
 
     //initializing buddy_allocator fields
     alloc->actual_memory = memory;
+
     alloc->memory_size = memory_size;
+
     alloc->levels = levels;
+
     alloc->chunk_size = min_chunk_size;
+
     bitmap*  bit_map =(bitmap*) buffer;
+
+
     //the bits are stored in the same buffer right after the bitmap structure
     bitmap_init(bit_map, total_nodes(levels) - 1, ((uint8_t*)buffer) + bitmap_structure_size(levels));
+
     alloc->bit_map = bit_map;
 
     return err;
 }
 
 
-//Sets as free (bit to 0) the block and proceeds to recursively free the parent if buddy is free (bit already set to 0)
+/* sets as free (bit to 0) the block and proceeds to recursively free the parent if buddy
+         is free (bit already set to 0) */
 void recursive_merge(buddy_allocator* alloc, int block){
+
     bitmap_set_bit(alloc->bit_map, block, 0);
+
     int block_buddy = buddy(block);
+
     //i.e block is the root
     if (block == 0) return;
+
     if (!bitmap_bit(alloc->bit_map, block_buddy)) recursive_merge(alloc, parent(block));
 }
 
 
+/* frees the block pointed by "mem" and recursively frees bigger blocks if possible */
 void buddy_free(buddy_allocator* alloc, void* mem){
 
     if (mem == NULL) return;
@@ -200,13 +243,13 @@ void buddy_free(buddy_allocator* alloc, void* mem){
     //mem is not a valid address
     if (distance % alloc->chunk_size != 0) return;
 
-    //Gets the node offset of "mem" from "start" expressed like the number of lowest level's chunks separing the two
+    //gets the node offset of "mem" from "start" expressed like the number of lowest level's chunks separing the two
     int node_offset = distance / alloc->chunk_size;
 
-    //Gets the index of the first node of the last level
+    //gets the index of the first node of the last level
     int first_node = first_of_level(alloc->levels-1);
 
-    //Gets the last level node's index that represents the start of mem
+    //gets the last level node's index that represents the start of mem
     int mem_node = first_node + node_offset;
 
     recursive_merge(alloc, mem_node);
--- disastrOS/project/tree.c
+++ disastrOS/project/tree.c
@@ -2,43 +2,59 @@
 #include <math.h>
 #include "assert.h"
 
-//returns the total number of nodes in the tree
+/* returns the total number of nodes in the tree */
 int total_nodes(int levels){
+
     return (1 << (levels)) - 1;
+
 }
 
-//returns the level the buddy is in
+/* returns the level the buddy is in */
 int get_level(int i){
+
   return floor(log2(i-1));
+
 }
 
-//returns the index of the level's first node
+/* returns the index of the level's first node */
 int first_of_level(int level){
+
     return (1 << level) - 1;
+
 }
 
-//returns the number of nodes in a level
+/* returns the number of nodes in a level */
 int nodes_in_level(int level){
+
     return 1 << level;
+
 }
 
-//returns the buddy
+/* returns the buddy of i*/
 int buddy(int i){
+
   return (i&0x01) ? i + 1 : i - 1;
+
 }
 
-//return the right child of i
+/* returns the right child of i */
 int right(int i){
+
   return 2 * i + 2;
+
 }
 
-//return the left child of i
+/* returns the left child of i */
 int left(int i){
+
   return 2 * i + 1;
+
 }
 
-//returns the parent of i
+/* returns the parent of i */
 int parent(int i){
+
   return floor((i-1) / 2);
+  
 }
 
